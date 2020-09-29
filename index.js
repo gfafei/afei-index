0d2468e19331b1f25fe01c5ff0d4d77fd7952d79
afei <afei_gf@foxmail.com>
2020-09-29 13:15:32 +0800

    index

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const express = require('express')
 const marked = require('marked')
 const fs = require('fs')
+const glob = require('glob')
+const config = require('./config')
 
 const app = express()
-const port = 3000
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    fs.readFile('./markdown.md', 'utf-8', (err, journal) => {
-        res.render('./pages/index', {
-            journal: marked(journal)
-        })
+    glob('**/*.md', { cwd: config.journalSrc }, (err, journals) => {
+        res.render('pages/index', { journals: journals.map(journal => journal.replace('.md', '')) })
     })
 })
-app.get('/:path', (req, res) => {
-    const path = req.params.path || '';
-    const file = path.replace('html', 'md');
-    if (fs.existsSync(`./mds/${file}`)) {
-        res.send('exists')
+app.get('/*.html', (req, res) => {
+    const path = req.path;
+    const file = path.replace('html', 'md')
+    const journalPath = `${config.journalSrc}/${file}`
+    if (fs.existsSync(journalPath)) {
+        fs.readFile(journalPath, 'utf-8', (err, data) => {
+            console.log(data);
+            res.render('pages/journal', { journal: marked(data) })
+        })
     } else {
         res.send('not exists')
     }
 })
-app.use(express.static('client'));
+app.use(express.static('./client'))
 
-app.listen(port, () => {
-    console.log('server listening on ', port)
+app.listen(config.port, () => {
+    console.log('server listening on ', config.port)
 })
